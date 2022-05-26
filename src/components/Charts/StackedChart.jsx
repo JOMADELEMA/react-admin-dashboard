5026cf52b0d0dd7b5a99a1e2a4dbe317869c54f1
JOMA-OFFICE <jomadelema@hotmail.com>
2022-05-26 16:30:57 -0600

    se implementó la pagina de Ecommerce

--- src/components/Charts/StackedChart.jsx
+++ src/components/Charts/StackedChart.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const StackedChart = () => {
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
+
+const StackedChart = ({width, height}) => {
   return (
-    <div>StackedChart</div>
-  )
-}
+    <ChartComponent 
+      width={width}
+      height={height}
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{border: {width: 0}}}
+      tooltip={{enable:true}}
+      legendSettings={{background: 'white'}}
 
-export default StackedChart
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item,index)=>
+          <SeriesDirective key={index} {...item}/> 
+        )}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
+};
+
+export default StackedChart;
