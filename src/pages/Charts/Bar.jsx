f3c853f355686b60db2ea1d60a0341e297c1ebbb
JOMA-OFFICE <jomadelema@hotmail.com>
2022-06-01 12:02:26 -0600

    se implementó el grafico de barras y el de pie

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  Tooltip,
+  BarSeries,
+  DataLabel,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  barChartData,
+  barPrimaryXAxis,
+  barPrimaryYAxis,
+} from "../../data/dummy";
+import { Header } from "../../components";
+
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Bar = () => {
-  return (
-    <div>Bar</div>
-  )
-}
 
-export default Bar
+  const {currentMode} = useStateContext();
+
+  return (
+    <div
+      className="m-4 md:m-10 mt-24 p-10 bg-white
+    dark:bg-secondary-dark-bg rounded-3xl "
+    >
+      <Header category="Chart" title="Bar Chart" />
+      <ChartComponent
+        id="bar-chart"
+        primaryXAxis={barPrimaryXAxis}
+        primaryYAxis={barPrimaryYAxis}
+        title="Grafico de ..."
+        tooltip={{enable: true}}
+        width="650px"
+        height="420px"
+        background={currentMode === "Dark" ? "#33373e" : "#fff"}
+      >
+        <Inject
+          services={[BarSeries, Legend, Tooltip, Category, DataLabel]}
+        />
+        <SeriesCollectionDirective>
+          {barChartData.map((item, index) => (
+            <SeriesDirective
+              key={index}
+              dataSource={item}
+              xName="x"
+              yName="y"
+              type="Bar"
+            />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
+};
+
+export default Bar;
--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  PieSeries,
+  Inject,
+  AccumulationDataLabel,
+} from "@syncfusion/ej2-react-charts";
+
+import { pieChartData } from "../../data/dummy";
+
+import { Header } from "../../components";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Pie = () => {
-  return (
-    <div>Pie</div>
-  )
-}
+  const { currentMode } = useStateContext();
 
-export default Pie
+    return (
+    <div
+      className="m-4 md:m-10 mt-24 p-10 bg-white
+  dark:bg-secondary-dark-bg rounded-3xl "
+    >
+      <Header category="Chart" title="Pie Chart" />
+      <AccumulationChartComponent
+        id="pie-chart"
+        height="420px"
+        width="650px"
+        background={currentMode === "Dark" ? "#33373e" : "#fff"}
+        enableSmartLabels="true"
+      >
+        <Inject services={[PieSeries, AccumulationDataLabel]} />
+        <AccumulationSeriesCollectionDirective>
+          <AccumulationSeriesDirective
+            dataSource={pieChartData}
+            pointColorMapping="fill"
+            dataLabel={{ visible: true, name: "x", position: "Outside"}}
+            xName="x"
+            yName="y"
+            type="Pie"
+          />
+        </AccumulationSeriesCollectionDirective>
+      </AccumulationChartComponent>
+    </div>
+  );
+};
+
+export default Pie;
